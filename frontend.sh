1113a34568cef494a95a4397e0e2d2139c84ca63
prathyusha <prathyusha0612@gmail.com>
2025-09-28 21:12:10 +0530

    creating instance

--- frontend.sh
+++ frontend.sh
@@ -11,12 +11,12 @@ NGINX_SETUP
 systemctl_enable_start
 
 rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>$LOG_FILE
+unzip /tmp/$app_name.zip &>>$LOG_FILE
 CHECK $? "Downloading frontend"
 
-cp /root/common-shell-roboshop/nginx.conf /etc/nginx/nginx.conf
+cp /root/common-shell_roboshop/nginx.conf /etc/nginx/nginx.conf
 CHECK $? "copying conf files"
 
 systemctl restart nginx 
